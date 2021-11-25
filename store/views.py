5fa70cb6a3e078ececc5485b6c9aab0ec4e696e5
l3va <ggwpleva@gmail.com>
2021-11-25 18:47:15 +0200

    provide dynamic data display and implement export products to csv functionality

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 
@@ -8,9 +11,39 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/checkout.html', context)
+
+
+def export_products_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="products.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['name', 'price'])
+
+    products = Product.objects.all().values_list('name', 'price')
+    for product in products:
+        writer.writerow(product)
+
+    return response
